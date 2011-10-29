05c276b16a95359e8d082878b49e3885f14bd056
Lyza Danger Gardner <lyza@lyza.com>
2011-10-29 14:40:34 -0700

    Re-worked step 2 master code. See notes.
    It's too much pain to try to maintain these steps in full. Step 2 only
    represents those files which have *changed* since step 1.

---
+++ 02_server_enhancement/README.markdown
@@ -0,0 +1,4 @@
+Note
+----
+
+Due to file management complexity, this is not a standalone step. Only the files reflecting changes from the previous step are included in this directory. Specifically, this is the addition of the `action` and `method` attributes on the `<form>` in `build.php` and the inclusion `inc/list.php` in `tartans.php`.
---
+++ 02_server_enhancement/tartans.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<meta charset="UTF-8" />
+	<title>The Tartanator</title> 
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.css" />
+  <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+  <script src="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.js"></script>
+  <link rel="stylesheet" href="css/styles.css" />
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header" data-position="fixed">
+		<h1>Popular Tartans</h1>
+		<a href="build.php" data-role="button" data-icon="plus" class="ui-btn-right" data-theme="b">Create</a>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+    <ul data-role="listview" data-filter="true">
+      <?php include('inc/list.php'); ?>
+    </ul>
+	</div><!-- /content -->
+	<div data-role="footer" data-position="fixed">
+		<div data-role="navbar"> 
+			<ul> 
+				<li><a href="index.php" data-icon="info">About</a></li> 
+				<li><a href="findevent.php" data-icon="star">Events</a></li> 
+				<li><a href="tartans.php" data-icon="grid" class="ui-btn-active">Tartans</a></li> 
+			</ul> 
+		</div><!-- /navbar --> 
+	</div><!-- /footer -->
+</div><!-- /page -->
+
+</body>
+</html>
