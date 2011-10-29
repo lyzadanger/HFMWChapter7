60a0670e9a815553711bc633dc9906947733a7ad
Lyza Danger Gardner <lyza@lyza.com>
2011-10-29 13:32:49 -0700

    Native select menu override has to be in every page because of JQM limitation.

--- 01_js_enhancement/tartans.php
+++ 01_js_enhancement/tartans.php
@@ -7,6 +7,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.css" />
   <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+  <script type="text/javascript">
+    // Need to bind to mobileinit before jQ mobile library is loaded
+    $(document).bind('mobileinit',function(){
+      $.mobile.selectmenu.prototype.options.nativeMenu = false;
+    });
+  </script>
   <script src="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.js"></script>
   <link rel="stylesheet" href="css/styles.css" />
 </head> 
