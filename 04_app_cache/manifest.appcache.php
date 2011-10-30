047f0e6cd17d53a948a69ae95e6d9a74c1c28edf
bryfox <bryan@bryfox.com>
2011-10-30 15:03:55 -0700

    Explicitly add detail HTML files to the manifest.
    We used to rely on lazy-loading, but this causes problem with the NETWORK wildcard whitelist, since loading a detail page from the list page will try to hit the network instead of using the cached version.
    
    Added this to a separate loop rather than futzing with filename strings (sorry, L, just seemed cleaner.)

--- 04_app_cache/manifest.appcache.php
+++ 04_app_cache/manifest.appcache.php
@@ -15,18 +15,34 @@ http://code.jquery.com/mobile/1.0rc1/images/icons-36-white.png
 http://code.jquery.com/mobile/1.0rc1/images/icons-36-black.png
 js/cache-manager.js
 css/styles.css
+
 <?php
 require_once('config.php');
-$dir = TARTAN_IMAGE_DIR;
-$tartans = array();
-if (is_dir($dir)) {
-  if ($dh = opendir($dir)) {
+
+// Cache the HTML files
+$html_dir = TARTAN_HTML_DIR;
+if (is_dir($html_dir)) {
+  if ($dh = opendir($html_dir)) {
     while (($file = readdir($dh)) !== false) {
-      if (filetype($dir . $file) === 'file' && strpos($file, '-200.png') !== FALSE) {
+      if (filetype($html_dir . $file) === 'file' && strpos($file, '.html') !== FALSE) {
+        print PUBLIC_TARTAN_DIR . $file . "\r\n";
+      }
+    }
+    closedir($dh);
+  }
+}
+
+// Cache the images
+$img_dir = TARTAN_IMAGE_DIR;
+if (is_dir($img_dir)) {
+  if ($dh = opendir($img_dir)) {
+    while (($file = readdir($dh)) !== false) {
+      if (filetype($img_dir . $file) === 'file' && strpos($file, '-200.png') !== FALSE) {
         print PUBLIC_TARTAN_DIR . 'images/' . $file . "\r\n";
       }
     }
     closedir($dh);
   }
 }
+
 ?>
