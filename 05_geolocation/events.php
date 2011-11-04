fe70730e06408853351e2a196ffd98cb94589baf
Lyza Danger Gardner <lyza@lyza.com>
2011-11-04 15:06:37 +0000

    Fixing a few egregious dumb errors in the find-event section

--- 05_geolocation/events.php
+++ 05_geolocation/events.php
@@ -8,7 +8,7 @@ if ($search->isSearch()): ?>
   <li data-role="list-divider"><h3>Matching Events</h3></li>
 <?php endif; ?>
 <?php if ($search->hasMatchingEvents()): ?>
-<? foreach($search->getMatchingEvents() as $event): ?>
+  <? foreach($search->getMatchingEvents() as $event): ?>
   <li><h3><?php print $event['title']; ?></h3>
   <p><?php print $event['address']; ?><br />
   <strong><?php print $event['city'] ?>, <?php print $event['state']; ?></strong> <?php print $event['postal_code']; ?><br />
@@ -16,6 +16,7 @@ if ($search->isSearch()): ?>
   <p class="ui-li-aside"><strong><?php print $event['date']; ?></strong></p>
   </li>
   <?php endforeach; ?>
-<?php else: ?>
+<?php endif; ?>
+<?php if ($search->isSearch() && !$search->hasMatchingEvents()): ?>
   <li><strong>Sorry, no matching events found!</strong></li>
 <?php endif; ?>
