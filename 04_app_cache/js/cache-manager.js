d18de940f50c9753bedba2b2068e2db6d28f0a32
bryfox <bryan@bryfox.com>
2011-11-05 03:07:38 -0700

    move event listener adding

--- 04_app_cache/js/cache-manager.js
+++ 04_app_cache/js/cache-manager.js
@@ -7,41 +7,41 @@
   var proto = {};
 
   proto.ensureFreshContent = function (selector, updateURL) {
-    this.selector  = selector;
-    this.updateURL = updateURL;
-    this.page.live('pageshow', $.proxy(this.checkCache, this));
-  };
-
-  proto.checkCache = function () {
     var appCache = window.applicationCache,
         showCached;
 
     // If browser doesn't support cache manifest, move along.
     if (!appCache) return;
 
+    this.selector  = selector;
+    this.updateURL = updateURL;
+    
     showCached = $.proxy(this.showCached, this);
+    
+    // If there's nothing to update, we'll show the cached version
+    appCache.addEventListener('cached', showCached, false);
+    appCache.addEventListener('error', showCached, false);
+    appCache.addEventListener('noupdate', showCached, false);
+    appCache.addEventListener('obsolete', showCached, false);
 
-    // Otherwise, hide the content until we know we have the latest
+    // But if we recognize an update, swap out the cache & display it.
+    appCache.addEventListener('updateready', $.proxy(this.updateCache, this), false);
+    
+    this.page.live('pageshow', $.proxy(this.checkCache, this));
+  };
+
+  proto.checkCache = function () {
+    // Hide the content until we know we have the latest
     $.mobile.showPageLoadingMsg();
     $(this.selector).hide();
 
-    if (!window.addedCacheListeners) {
-      // If there's nothing to update, we'll show the cached version
-      appCache.addEventListener('cached', showCached, false);
-      appCache.addEventListener('error', showCached, false);
-      appCache.addEventListener('noupdate', showCached, false);
-      appCache.addEventListener('obsolete', showCached, false);
-
-      // But if we recognize an update, swap out the cache & display it.
-      appCache.addEventListener('updateready', $.proxy(this.updateCache, this), false);
-      window.addedCacheListeners = true;
-    }
-
     // Kick off the update
     try {
-      appCache.update();
+      window.applicationCache.update();
     } catch (e) {
-      this.updateCache();
+      // If it failed, just show the cached data.
+      // We could call this.updateCache() here, but this may fire on first load.
+      this.showCached();
     }
   };
 
